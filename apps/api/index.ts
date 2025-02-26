95a3c4686dd067a3edabfd733e2c3750f199ccbf
Judit Greskovits <329441+juditgreskovits@users.noreply.github.com>
2025-02-26 15:54:41 +0000

    feat: add server

---
+++ apps/api/index.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Express + TypeScript Server for Judit');
+});
+
+app.get('/health', (_req: Request, res: Response) => {
+  res.status(200).send();
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
